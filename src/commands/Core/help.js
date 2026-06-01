dfd29479ec2233d32207da7003f9b58b7baf9eed
idbuilderdev-coder <idbuilderdev@gmail.com>
2026-06-01 10:06:29 +0700

    Update help.js

--- src/commands/Core/help.js
+++ src/commands/Core/help.js
@@ -158,7 +158,7 @@ export async function createInitialHelpMenu(client) {
     );
 
     embed.setFooter({ 
-        text: "Made with ❤️" 
+        text: "Made By : BuilderDev" 
     });
     embed.setTimestamp();
 
